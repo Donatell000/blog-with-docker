2d9263eae8caed70c43a5e4482bc037df5bfd84f
adm <admin@adm.com>
2024-11-04 07:08:20 +0500

    тест

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     description = models.TextField(blank=True)
     age = models.IntegerField(null=True, blank=True)
 
+
     def __str__(self):
         return self.user.username
 
